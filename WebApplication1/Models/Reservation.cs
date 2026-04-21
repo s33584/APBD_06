b17b0d4073db1de8fbf0354d89f801782029fe2b
Arseni Mahulenka <s33584@pjwstk.edu.pl>
2026-04-21 11:14:35 +0200

    reservation + room models

---
+++ WebApplication1/Models/Reservation.cs
@@ -0,0 +1,26 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1.Models;
+
+public class Reservation
+{
+    public int Id { get; set; }
+
+    public int RoomId { get; set; }
+
+    [Required]
+    [MinLength(1)]
+    public string OrganizerName { get; set; } = string.Empty;
+
+    [Required]
+    [MinLength(1)]
+    public string Topic { get; set; } = string.Empty;
+
+    public DateOnly Date { get; set; }
+
+    public TimeOnly StartTime { get; set; }
+
+    public TimeOnly EndTime { get; set; }
+
+    public string Status { get; set; } = string.Empty;
+}
